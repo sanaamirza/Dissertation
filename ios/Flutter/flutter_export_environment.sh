daeb21f3f42b6a4dc2a4b9571b96b1f08a180d81
sanaamirza <78091442+sanaamirza@users.noreply.github.com>
2023-03-10 19:59:44 +0000

    Commit 2
    Initial commit 2

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/Sanaa 1/Documents/Sanaa/Uni/Year 3/Disso/App/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/Sanaa 1/Documents/Sanaa/Uni/Year 3/Disso/App/my_app/my_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/Sanaa 1/Documents/Sanaa/Uni/Year 3/Disso/App/my_app/my_app/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/Sanaa 1/Documents/Sanaa/Uni/Year 3/Disso/App/my_app/my_app/.dart_tool/package_config.json"
---
+++ lib/windows/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart'
+
+class Login extends StatefulWidget {
+  conss Login({super.key});
+
+  @override
+  Stats Login> createState() =>s LoginState();
+}
+
+classs LoginState extends Stats Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
